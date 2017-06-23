11c6f61d51baf6544730ca3f89c2347721b3529e
Edgar <edgargmc@gmail.com>
2017-06-23 10:48:25 -0300

    change template email

--- app/helpers/Helpers.php
+++ app/helpers/Helpers.php
@@ -104,7 +104,33 @@ class Helpers{
         $headers .= "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: text/html; charset=UTF-8";
 
-        $message =  file_get_contents('http://localhost/module_users/app/helpers/template.html');
+        $message =  '
+        <form id="login-form" action="post" method="" role="form" style="display: block;">
+    <div class="form-group">
+        <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="" required>
+    </div>
+    <div class="form-group">
+        <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password" required>
+    </div>
+    <div class="form-group">
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
+            </div>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="text-center" >
+                    <a href="" tabindex="5" class="forgot-password" id="forgotpass">Recover Password?</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+        
+        ';
 
         if(!mail($to,$subject,$message, $headers)){
             return false;
